c3832dcdd8a29bb4a65ec3b84cce47ec2b9d07cc
Iryna <irinabolbat98@gmail.com>
2024-05-30 20:36:55 +0300

    add calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyCalculatorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-course';
-}
+export class AppComponent {}
